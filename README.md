4be9d696e71cf62bb113d4956159aeabc03fb1e7
langheran <langheran@gmail.com>
2019-05-31 18:22:01 -0500

    changes

---
+++ README.md
@@ -0,0 +1,11 @@
+## Minimal CMakeLists C++ VSCode - MSVC+Ninja
+
+Steps to reproduce minimal functionality:
+
+1. Install `ms-vscode.cpptools` extension
+2. Install `vector-of-bool.cmake-tools` extension
+3. Ctrl+Shift+P `CMake: Select Kit` -> VisualStudio.14.0 - amd64 (Ninja is set on workspace settings)
+4. Ctrl+Shift+P `CMake: Set current build variant` -> Debug
+5. Ctrl+Shift+P `CMake: Delete cached build settings and reconfigure`
+6. Ctrl+Shift+P `CMake: Clean rebuild`
+7. F5 to Debug the first task on `launch.json`, `build` on `tasks.json` must also be executed
--- minimal.cpp
+++ minimal.cpp
@@ -5,12 +5,13 @@ using namespace std;
 int main()
 {
     /** Steps to reproduce minimal functionality:
-     * 1. Install ms-vscode.cpptools
-     * 2. Install vector-of-bool.cmake-tools
-     * 3. Ctrl+Shift+P `CMake: Select Kit` -> VisualStudio.14.0 - amd64
-     * 4. Ctrl+Shift+P `CMake: Delete cached build settings and reconfigure`
-     * 5. Ctrl+Shift+P `CMake: Clean rebuild`
-     * 6. F5 to Debug the first task on `launch.json`, `build` on `tasks.json` must also be executed
+     * 1. Install `ms-vscode.cpptools` extension
+     * 2. Install `vector-of-bool.cmake-tools` extension
+     * 3. Ctrl+Shift+P `CMake: Select Kit` -> VisualStudio.14.0 - amd64 (Ninja is set on workspace settings)
+     * 4. Ctrl+Shift+P `CMake: Set current build variant` -> Debug
+     * 5. Ctrl+Shift+P `CMake: Delete cached build settings and reconfigure`
+     * 6. Ctrl+Shift+P `CMake: Clean rebuild`
+     * 7. F5 to Debug the first task on `launch.json`, `build` on `tasks.json` must also be executed
      */
     cout << "Minimal";
     cin.get();
